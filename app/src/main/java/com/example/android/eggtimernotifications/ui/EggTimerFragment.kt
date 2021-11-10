9741a49d7f525142d65ca26795af0d4506693d10
oscar <oscar.goodwin@gmail.com>
2021-11-10 08:18:42 +0800

    Push test message fromFirebase Cloud Messaging

--- app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerFragment.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerFragment.kt
@@ -56,6 +56,12 @@ class EggTimerFragment : Fragment() {
             getString(R.string.egg_notification_channel_name)
         )
 
+        //FCM push notification channel
+        createChannel(
+            getString(R.string.breakfast_notification_channel_id),
+            getString(R.string.breakfast_notification_channel_name)
+        )
+
         return binding.root
     }
 
